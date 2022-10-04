b16aae339e7f5647f7bc08931e6fcbec6dc05fe8
omar2205 <omar2205a@yahoo.com>
2022-10-04 02:18:59 +0200

    add latest

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -19,22 +19,22 @@ echo "Go version ${INSTALLED_GO_VERSION} is installed"
 
 # install gopls, dlv, hey
 echo "Getting development tools"
-go install golang.org/x/tools/gopls
-go install github.com/go-delve/delve/cmd/dlv
-go install github.com/rakyll/hey
+go install golang.org/x/tools/gopls@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/rakyll/hey@latest
 
 # vscode-go dependencies 
 echo "Getting dependencies for the vscode-go plugin "
 # via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
-go install github.com/acroca/go-symbols
-go install github.com/cweill/gotests/...
-go install github.com/davidrjenni/reftools/cmd/fillstruct
-go install github.com/haya14busa/goplay/cmd/goplay
-go install github.com/mdempsky/gocode
-go install github.com/ramya-rao-a/go-outline
-go install github.com/rogpeppe/godef
-go install github.com/sqs/goreturns
-go install github.com/uudashr/gopkgs/cmd/gopkgs
-go install github.com/zmb3/gogetdoc
-go install golang.org/x/lint/golint
-go install golang.org/x/tools/cmd/gorename
+go install github.com/acroca/go-symbols@latest
+# go install github.com/cweill/gotests/...
+go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
+go install github.com/haya14busa/goplay/cmd/goplay@latest
+go install github.com/mdempsky/gocode@latest
+go install github.com/ramya-rao-a/go-outline@latest
+go install github.com/rogpeppe/godef@latest
+go install github.com/sqs/goreturns@latest
+go install github.com/uudashr/gopkgs/cmd/gopkgs@latest
+go install github.com/zmb3/gogetdoc@latest
+go install golang.org/x/lint/golint@latest
+go install golang.org/x/tools/cmd/gorename@latest
